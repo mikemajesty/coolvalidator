ae4d3ec96a0f733b3919e039e4be23102db76b97
mikemajesty <mikemajesty@gmail.com>
2016-10-21 19:55:34 -0200

    Add get textbox

--- CoolValidator/Validator.cs
+++ CoolValidator/Validator.cs
@@ -1,17 +1,23 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Windows.Forms;
 
 namespace CoolValidator
 {
-    public static class Validator
+    public static class formValidator
     {
-
-        public static IEnumerable<TextBox> GetTextBoxInComponent(this Form form, ComponentType type)
+        public static List<TextBox> GetTextBoxInComponent(this Form form, Func<TextBox, bool> predicate)
         {
-            return form.Controls.OfType<Panel>().SelectMany(groupBox => groupBox.Controls.OfType<TextBox>()).OrderBy(t => t.TabIndex);
+            var txtList = new List<TextBox>();
+            var txtInPanel = form.Controls.OfType<Panel>().SelectMany(groupBox => groupBox.Controls.OfType<TextBox>()).OrderBy(t => t.TabIndex).Where(c => string.IsNullOrEmpty(c.Text)).Where(predicate).ToList();
+            var txtInManyPanel = form.Controls.OfType<Panel>().SelectMany(groupBox => groupBox.Controls.OfType<Panel>()).SelectMany(textBox => textBox.Controls.OfType<TextBox>()).OrderBy(t => t.TabIndex).Where(c => string.IsNullOrEmpty(c.Text)).Where(predicate).ToList();
+            var txtInForm = form.Controls.OfType<TextBox>().Where(c => string.IsNullOrEmpty(c.Text)).Where(predicate).ToList();
+            txtList.AddRange(txtInPanel);
+            txtList.AddRange(txtInManyPanel);
+            txtList.AddRange(txtInForm);
+            return txtList;
         }
-
     }
 
 }
