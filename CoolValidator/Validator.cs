26f9b88b1292e64e0bc09e3f2f14809c93b05132
mikemajesty <mikemajesty2011@gmail.com>
2016-10-22 19:24:10 -0200

    Refactor validator

--- CoolValidator/Validator.cs
+++ CoolValidator/Validator.cs
@@ -12,13 +12,28 @@ namespace CoolValidator
             where T : Control
         {
             var txtList = new List<TextBox>();
-            var txtInPanel = form.Controls.OfType<T>().SelectMany(groupBox => groupBox.Controls.OfType<TextBox>()).OrderBy(t => t.TabIndex).Where(c => string.IsNullOrEmpty(c.Text.Trim())).ToList();
-            var txtInManyPanel = form.Controls.OfType<T>().SelectMany(groupBox => groupBox.Controls.OfType<T>()).SelectMany(textBox => textBox.Controls.OfType<TextBox>()).OrderBy(t => t.TabIndex).Where(c => string.IsNullOrEmpty(c.Text.Trim())).ToList();
-            var txtInForm = form.Controls.OfType<TextBox>().Where(c => string.IsNullOrEmpty(c.Text.Trim())).ToList();
+            var txtInPanel = GetTextBoxInContainer<T>(form);
+            var txtInManyPanel = GetTextBoxHierarchicalContainer<T>(form);
+            var txtInForm = GetTextBoxInForm(form);
             txtList.AddRange(txtInPanel);
             txtList.AddRange(txtInManyPanel);
             txtList.AddRange(txtInForm);
             return predicate == null ? txtList : txtList.Where(predicate).ToList();
+        }
+
+        private static List<TextBox> GetTextBoxInForm(Form form)
+        {
+            return form.Controls.OfType<TextBox>().Where(c => string.IsNullOrEmpty(c.Text.Trim())).ToList();
+        }
+
+        private static List<TextBox> GetTextBoxHierarchicalContainer<T>(Form form) where T : Control
+        {
+            return form.Controls.OfType<T>().SelectMany(groupBox => groupBox.Controls.OfType<T>()).SelectMany(textBox => textBox.Controls.OfType<TextBox>()).OrderBy(t => t.TabIndex).Where(c => string.IsNullOrEmpty(c.Text.Trim())).ToList();
+        }
+
+        private static List<TextBox> GetTextBoxInContainer<T>(Form form) where T : Control
+        {
+            return form.Controls.OfType<T>().SelectMany(groupBox => groupBox.Controls.OfType<TextBox>()).OrderBy(t => t.TabIndex).Where(c => string.IsNullOrEmpty(c.Text.Trim())).ToList();
         }
     }
 
