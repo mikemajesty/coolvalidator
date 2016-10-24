f4f030437304bcbb962da1db199798df60e436a7
mikemajesty <mikemajesty@gmail.com>
2016-10-24 12:30:53 -0200

    Add message class

---
+++ CoolValidator/MessageInfo.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows.Forms;
+
+namespace CoolValidator
+{
+    public class MessageInfo
+    {
+        public string Text { get; set; }
+        public string Caption { get; set; }
+        public MessageBoxIcon icon { get; set; }
+    }
+}
