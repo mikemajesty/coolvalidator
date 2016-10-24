38a8298abf1ae20314676e2e3ad64f106836e15d
mikemajesty <mikemajesty2011@gmail.com>
2016-10-23 23:00:50 -0200

    Fix bug

--- CoolValidator/Validator.cs
+++ CoolValidator/Validator.cs
@@ -12,17 +12,22 @@ namespace CoolValidator
         {
             var txtList = new List<TextBox>();
 
-            var txtInPanel = GetTextBoxInGroupBox(form);
+            var txtInPanel = GetTextBoxInPanel(form);
             var txtInManyPanel = GetInHierarchicalPanel(form);
 
             var txtInGroupBox = GetTextBoxInGroupBox(form);
             var txtInManyGroupBox = GetInHierarchicalGroupBox(form);
 
             var txtInForm = GetTextBoxInForm(form);
+
+            txtList.AddRange(txtInGroupBox);
+            txtList.AddRange(txtInManyGroupBox);
+
             txtList.AddRange(txtInPanel);
             txtList.AddRange(txtInManyPanel);
+
             txtList.AddRange(txtInForm);
-            return predicate == null ? txtList : txtList.Where(predicate).ToList();
+            return predicate == null ? txtList.OrderBy(t => t.TabIndex).ToList() : txtList.Where(predicate).OrderBy(t => t.TabIndex).ToList();
         }
 
         private static List<TextBox> GetTextBoxInForm(Form form)
@@ -30,25 +35,24 @@ namespace CoolValidator
             return form.Controls.OfType<TextBox>().Where(c => string.IsNullOrEmpty(c.Text.Trim())).ToList();
         }
 
-        private static List<TextBox> GetInHierarchicalPanel(Form form)
-        {
-            return form.Controls.OfType<Panel>().SelectMany(panel => panel.Controls.OfType<Panel>()).SelectMany(textBox => textBox.Controls.OfType<TextBox>()).OrderBy(t => t.TabIndex).Where(c => string.IsNullOrEmpty(c.Text.Trim())).ToList();
-        }
-
         private static List<TextBox> GetTextBoxInGroupBox(Form form)
         {
-            return form.Controls.OfType<Panel>().SelectMany(panel => panel.Controls.OfType<TextBox>()).OrderBy(t => t.TabIndex).Where(c => string.IsNullOrEmpty(c.Text.Trim())).ToList();
+            return form.Controls.OfType<GroupBox>().SelectMany(panel => panel.Controls.OfType<TextBox>()).Where(c => string.IsNullOrEmpty(c.Text.Trim())).ToList();
         }
 
         private static List<TextBox> GetInHierarchicalGroupBox(Form form)
         {
-            return form.Controls.OfType<GroupBox>().SelectMany(panel => panel.Controls.OfType<GroupBox>()).SelectMany(textBox => textBox.Controls.OfType<TextBox>()).OrderBy(t => t.TabIndex).Where(c => string.IsNullOrEmpty(c.Text.Trim())).ToList();
+            return form.Controls.OfType<GroupBox>().SelectMany(panel => panel.Controls.OfType<GroupBox>()).SelectMany(textBox => textBox.Controls.OfType<TextBox>()).Where(c => string.IsNullOrEmpty(c.Text.Trim())).ToList();
         }
 
-        private static List<TextBox> dfdfd(Form form)
+        private static List<TextBox> GetTextBoxInPanel(Form form)
         {
-            return form.Controls.OfType<GroupBox>().SelectMany(panel => panel.Controls.OfType<TextBox>()).OrderBy(t => t.TabIndex).Where(c => string.IsNullOrEmpty(c.Text.Trim())).ToList();
+            return form.Controls.OfType<Panel>().SelectMany(panel => panel.Controls.OfType<TextBox>()).Where(c => string.IsNullOrEmpty(c.Text.Trim())).ToList();
+        }
+
+        private static List<TextBox> GetInHierarchicalPanel(Form form)
+        {
+            return form.Controls.OfType<Panel>().SelectMany(panel => panel.Controls.OfType<Panel>()).SelectMany(textBox => textBox.Controls.OfType<TextBox>()).Where(c => string.IsNullOrEmpty(c.Text.Trim())).ToList();
         }
     }
-
 }
